edc577a27c42d9fafc803dd3806cfaa48a82d2b7
MayonaiseMan <tommaso.brandinelli@gmail.com>
2020-10-07 11:06:44 +0200

    creazione array, riempimento 1-100

---
+++ SortVector/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SortVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vs = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < vs.Length; i++)
+                vs[i] = rnd.Next(1,101);
+
+            
+
+            for (int i = 0; i < vs.Length; i++)
+                Console.WriteLine(vs[i]);
+
+            Console.ReadKey();
+
+            
+        }
+
+        
+    }
+}
